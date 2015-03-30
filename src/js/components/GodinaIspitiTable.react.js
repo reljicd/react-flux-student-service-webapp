e26299d410277bbd9392cbb3aa1fc29c0421cf46
Dušan Reljić <dusan.reljic@etag.co.rs>
2015-03-30 14:21:46 +0200

    Implemented "Kratak Pregled" tab page

---
+++ src/js/components/GodinaIspitiTable.react.js
@@ -0,0 +1,90 @@
+/**
+ * Created by reljicd on 3/30/15.
+ */
+
+var React = require('react');
+var ReactBootstrap = require('react-bootstrap'),
+    Panel = ReactBootstrap.Panel,
+    Col = ReactBootstrap.Col,
+    Row = ReactBootstrap.Row,
+    Input = ReactBootstrap.Input,
+    Table = ReactBootstrap.Table;
+var LabelAndDisabledInputText = require('../components/LabelAndDisabledInputText.react');
+
+var GodinaIspitiTable = React.createClass({
+
+    render: function () {
+        var godina = this.props.godina;
+        var godinaLabel;
+
+        switch (this.props.godina) {
+
+            case '1':
+                godinaLabel = 'Prva';
+                break;
+            case '2':
+                godinaLabel = 'Druga';
+                break;
+            case '3':
+                godinaLabel = 'Treca';
+                break;
+            case '4':
+                godinaLabel = 'Cetvrta';
+                break;
+            default:
+                godinaLabel = '';
+                break;
+        }
+
+        return (
+            <Panel id='godinaIspitiPanel'>
+                <h4>{godinaLabel} godina</h4>
+                <Table striped bordered condensed hover>
+                    <thead>
+                        <tr>
+                            <th>Predmet</th>
+                            <th>E</th>
+                            <th>O</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Jacob</td>
+                            <td>T</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Jacob</td>
+                            <td>T</td>
+                        </tr>
+                    </tbody>
+                </Table>
+                <span className='bottom'>
+                    <Col md={6}>
+                        <p className='textAllignedLeftSpecial'>Ukupno</p>
+                        <p className='textAllignedLeftSpecial'>Pol. ispita</p>
+                    </Col>
+                    <Col md={6}>
+                        <Row>
+                            <Col md={6}>
+                                <Input type='text' className='superNarrowInputText' disabled/>
+                            </Col>
+                            <Col md={6}>
+                                <Input type='text' className='superNarrowInputText' disabled/>
+                            </Col>
+                        </Row>
+                        <Input type='text' className='superNarrowInputText' id='polIspitaRightAlignedInputText' disabled/>
+                    </Col>
+                </span>
+            </Panel>
+        );
+    }
+});
+
+module.exports = GodinaIspitiTable;
--- src/js/components/KratakPregled.react.js
+++ src/js/components/KratakPregled.react.js
@@ -3,12 +3,57 @@
  */
 
 var React = require('react');
+var ReactBootstrap = require('react-bootstrap'),
+    Row = ReactBootstrap.Row,
+    Button = ReactBootstrap.Button,
+    Panel = ReactBootstrap.Panel,
+    Glyphicon = ReactBootstrap.Glyphicon,
+    Col = ReactBootstrap.Col;
+var LabelAndDisabledInputText = require('../components/LabelAndDisabledInputText.react');
+var GodinaIspitiTable = require('../components/GodinaIspitiTable.react');
 
 var KratakPregled = React.createClass({
 
     render: function () {
         return (
-            <h1>Kratak Pregled</h1>
+            <span>
+                < Row >
+                    <Col md={3}>
+                        <GodinaIspitiTable godina='1'/>
+                    </Col>
+                    <Col md={3}>
+                        <GodinaIspitiTable godina='2'/>
+                    </Col>
+                    <Col md={3}>
+                        <GodinaIspitiTable godina='3'/>
+                    </Col>
+                    <Col md={3}>
+                        <GodinaIspitiTable godina='4'/>
+                    </Col>
+                </Row>
+                < Row >
+                    <Col md={10}>
+                        <Panel>
+                            <Col md={4}>
+                                <LabelAndDisabledInputText label='ESPB' inputTextValue='' narrow/>
+                                <LabelAndDisabledInputText label='Ukupno ESPB' inputTextValue='' narrow/>
+                            </Col>
+                            <Col md={4}>
+                                <LabelAndDisabledInputText label='Prosecna ocena' inputTextValue='' narrow/>
+                                <LabelAndDisabledInputText label='Uk. pros. ocena' inputTextValue='' narrow/>
+                            </Col>
+                            <Col md={4}>
+                                <LabelAndDisabledInputText label='Polozeno ispita' inputTextValue='' narrow/>
+                            </Col>
+                        </Panel>
+                    </Col>
+                    <Col md={2}>
+                        <Button bsStyle='primary' id='kratakPregledIzlazButton'>
+                            <Glyphicon glyph='remove' />
+                            Izlaz</Button>
+                    </Col>
+                </ Row >
+            </span>
         );
     }
 });
