8366e87794d4b3d41fc823a73de25f58c1d7ae39
Dušan Reljić <reljicd@gmail.com>
2015-03-31 21:27:05 +0200

    Propagating of CHOOSE_STUDENT action, and subsequent change in Student Store of studentId value

--- src/js/actions/StudentActionCreators.js
+++ src/js/actions/StudentActionCreators.js
@@ -2,3 +2,19 @@
  * Created by reljicd on 3/31/15.
  */
 
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var Constants = require('../constants/Constants');
+
+var ActionTypes = Constants.ActionTypes;
+
+module.exports = {
+
+    changeStudent: function(studentId) {
+        console.log("Action CHOOSE_STUDENT for studentId: " + studentId);
+        AppDispatcher.dispatch({
+            type: ActionTypes.CHOOSE_STUDENT,
+            studentId: studentId
+        });
+    }
+
+};
--- src/js/stores/StudentStore.js
+++ src/js/stores/StudentStore.js
@@ -12,9 +12,10 @@ var ActionTypes = Constants.ActionTypes;
 var CHANGE_EVENT = 'change';
 
 var _students = {};
+var _chosenStudentID;
 
 function _addStudents(rawStudents) {
-    rawStudents.forEach(function(message) {
+    rawStudents.forEach(function (message) {
         if (!_students[message.id]) {
             _students[message.id] = StudentUtils.convertRawStudent(
                 message
@@ -23,40 +24,62 @@ function _addStudents(rawStudents) {
     });
 }
 
+function _changeChosenStudentID(studentId) {
+    console.log("Changed studentId to: " + studentId);
+    _chosenStudentID = studentId;
+}
+
 var studentStore = assign({}, EventEmitter.prototype, {
 
-    emitChange: function() {
+    emitChange: function () {
         this.emit(CHANGE_EVENT);
-        console.log('Loaded All Students from Memory: ' + JSON.stringify(this.getAll()));
+      //  console.log('Loaded All Students from Memory: ' + JSON.stringify(this.getAll()));
     },
 
     /**
      * @param {function} callback
      */
-    addChangeListener: function(callback) {
+    addChangeListener: function (callback) {
         this.on(CHANGE_EVENT, callback);
     },
 
-    removeChangeListener: function(callback) {
+    removeChangeListener: function (callback) {
         this.removeListener(CHANGE_EVENT, callback);
     },
 
-    get: function(id) {
-        return _students[id];
+    /**
+     * @param {string} id
+     */
+    get: function (id) {
+        if (id === undefined) {
+            return _students[27];
+        } else {
+            return _students[id];
+        }
+
     },
 
-    getAll: function() {
+    getAll: function () {
         return _students;
     },
 
+    getChosenStudentID: function () {
+        return _chosenStudentID;
+    }
+
 });
 
-studentStore.dispatchToken = AppDispatcher.register(function(action) {
+studentStore.dispatchToken = AppDispatcher.register(function (action) {
 
-    switch(action.type) {
+    switch (action.type) {
 
         case ActionTypes.RECEIVE_RAW_STUDENTS:
             _addStudents(action.rawStudents);
+            studentStore.emitChange();
+            break;
+
+        case ActionTypes.CHOOSE_STUDENT:
+            _changeChosenStudentID(action.studentId);
             studentStore.emitChange();
             break;
 
